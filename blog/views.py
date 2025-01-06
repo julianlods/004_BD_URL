3a6f60e0d960e66e408a1fb4996454cb3f4b403e
Julian Lods <julian.lods@gmail.com>
2025-01-06 00:20:20 -0300

    Archivos del proyecto

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/post_list.html', context={"posts": post_list})
+
